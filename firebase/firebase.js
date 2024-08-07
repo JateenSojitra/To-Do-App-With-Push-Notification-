43bd444c0511f64e433ca065a3324d70b77946c9
Jatin <jatinsojitra21@gmail.com>
2024-08-07 23:49:14 +0530

    Create todo Basic funcationallty

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,7 +1,6 @@
 // fristfirebaseproject-fb0dbs
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,8 +15,5 @@ const firebaseConfig = {
   appId: "1:490834359663:web:e390266ab326342fd7a49a",
   measurementId: "G-3L2HM3PMS9"
 };
-
-// const validkey = "BDQcXS8sSA9BQgiFRu2-0aLx0galzcCOoIns8U3ablqXlmKXevBSQz_nv6G0_gWVeeeu-EM1A03_xKN-T4tAegw"
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
